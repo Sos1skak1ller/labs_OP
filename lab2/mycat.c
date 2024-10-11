172a9232f9cebd57965cd2daa3762a9176d0897d
bykovskiy_m_r <misha.bykovskij@bk.ru>
2024-10-11 15:02:02 +0300

    правки на паре

--- lab2/mycat.c
+++ lab2/mycat.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void print_file(FILE *file, int show_line_num, int show_nonempty_line_num, int show_ends) {
     char line[1024];
     int line_num = 1;
     int non_empty_line_num = 1;
-    
+
     while (fgets(line, sizeof(line), file)) {
         int is_non_empty = strlen(line) > 1; // Проверка на непустую строку
 
@@ -30,30 +31,27 @@ void print_file(FILE *file, int show_line_num, int show_nonempty_line_num, int s
 int main(int argc, char *argv[]) {
     int show_line_num = 0, show_nonempty_line_num = 0, show_ends = 0;
     char *filename = NULL;
-
-    // Обработка аргументов командной строки
-    for (int i = 1; i < argc; i++) {
-        switch (argv[i][0]) {
-            case '-':
-                switch (argv[i][1]) {
-                    case 'n':
-                        show_line_num = 1;
-                        break;
-                    case 'b':
-                        show_nonempty_line_num = 1;
-                        break;
-                    case 'E':
-                        show_ends = 1;
-                        break;
-                    default:
-                        fprintf(stderr, "Unknown option: %s\n", argv[i]);
-                        return 1;
-                }
+    int opt;
+    while ((opt = getopt(argc, argv, "nEb")) != -1) {
+        switch (opt) {
+            case 'n':
+                show_line_num = 1;
+                break;
+            case 'b':
+                show_nonempty_line_num = 1;
+                break;
+            case 'E':
+                show_ends = 1;
                 break;
             default:
-                filename = argv[i];
-                break;
+                fprintf(stderr, "Usage: %s [-n|-b|-E] file\n", argv[0]);
+                return 1;
         }
+    }
+
+    // Получение имени файла после обработки флагов
+    if (optind < argc) {
+        filename = argv[optind];
     }
 
     // Проверка на наличие файла в аргументах
