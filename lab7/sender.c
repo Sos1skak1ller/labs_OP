6c9d3d9cc8cff2a3a5dace3d0ebe6a60fd519303
bykovskiy_m_r <misha.bykovskij@bk.ru>
2024-12-13 14:53:55 +0300

    вроде пофиксил lab 7

--- lab7/sender.c
+++ lab7/sender.c
@@ -4,14 +4,22 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <time.h>
+#include <fcntl.h>
 
 #define SHM_SIZE 128
-#define LOCK_FILE "/tmp/sender.lock"
 #define SHM_KEY_PATH "/tmp/mem_key"
+#define LOCK_FILE "/tmp/sender.lock"
 
 int main() {
+    // Create the lock file dynamically
+    int lock_fd = open(LOCK_FILE, O_CREAT | O_RDWR, 0666);
+    if (lock_fd == -1) {
+        perror("open LOCK_FILE");
+        exit(1);
+    }
+    close(lock_fd);
+
     int fd = open(SHM_KEY_PATH, O_CREAT | O_RDWR, 0666);
     if (fd == -1) {
         perror("open SHM_KEY_PATH");
@@ -19,24 +27,13 @@ int main() {
     }
     close(fd);
 
-    int lock_fd = open(LOCK_FILE, O_CREAT | O_RDWR, 0666);
-    if (lock_fd == -1) {
-        perror("open LOCK_FILE");
-        exit(1);
-    }
-
-    if (lockf(lock_fd, F_TLOCK, 0) == -1) {
-        printf("Sender already running. Exiting...\n");
-        exit(0);
-    }
-
     key_t key = ftok(SHM_KEY_PATH, 'a');
     if (key == -1) {
         perror("ftok");
         exit(1);
     }
 
-    int shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666);
+    int shmid = shmget(key, SHM_SIZE, 0666);
     if (shmid == -1) {
         perror("shmget");
         exit(1);
@@ -49,10 +46,12 @@ int main() {
     }
 
     pid_t pid = getpid();
+
     while (1) {
         time_t current_time = time(NULL);
-        snprintf(shmaddr, SHM_SIZE, "Time: %sPID: %d", ctime(&current_time), pid);
-        printf("Written to shared memory: Time: %sPID: %d\n", ctime(&current_time), pid);
+        char time_str[64];
+        strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", localtime(&current_time));
+        printf("Current Time: %s, PID: %d, Received: %s\n", time_str, pid, shmaddr);
         sleep(1);
     }
 
@@ -60,8 +59,6 @@ int main() {
         perror("shmdt");
         exit(1);
     }
-    close(lock_fd);
-    unlink(LOCK_FILE);
 
     return 0;
 }
